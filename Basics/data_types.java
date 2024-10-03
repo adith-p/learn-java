cdd7cbc3aa2394ea9b893f2ae2b1eb71dd62a1fa
adith-p <adithprakash008@gmail.com>
2024-10-03 18:57:10 +0530

    added explanations to the existing code

--- Basics/data_types.java
+++ Basics/data_types.java
@@ -1,9 +1,19 @@
 class Main {
-	public static void main(String[] ar) {
+	public static void main(String[] arg) {
+
 		int a = 12;
+
+		// 'double' can store numbers with decimal points.
 		double d = 23.25;
+
+		// A char represents a single character in Java and is enclosed in single quotes
+
 		char c = 'C';
+
+		// A String is a sequence of characters and is enclosed in double quotes.
 		String s = "hello world";
+
+		// At this point, we haven't printed any output, but the variables have been
+		// initialized with values.
 	}
 }
-
